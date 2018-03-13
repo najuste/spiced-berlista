3f1d4552b39bdeda99179fa63a4acc1e564cc3c8
najuste <najuste@gmail.com>
2018-03-13 10:38:33 +0100

    Hello from Justina

--- src/start.js
+++ src/start.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-    <HelloWorld />,
-    document.querySelector('main')
-);
+ReactDOM.render(<HelloWorld />, document.querySelector("main"));
 
 function HelloWorld() {
-    return (
-        <div>Hello, World!</div>
-    );
+    console.log("Hello from Justina");
+    return <div>Hello, World!</div>;
 }
